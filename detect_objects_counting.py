ccfb2e78c40633622e36336fb76a50c5c070a65c
Salvador Jiménez Prats <sjimenez@sigsa.info>
2024-04-26 17:49:54 -0600

    refactoring...

--- detect_objects_counting.py
+++ detect_objects_counting.py
@@ -73,136 +73,24 @@ def mouse_callback(event, x, y, flags, param):
             current_region["dragging"] = False
 
 
-def setup_model():
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    model = YOLO(YOLO_MODEL)
-    return model, device
-
-
-def setup_video_source(webcam):
-    video_source = 0 if webcam else VIDEO_SOURCE
-    capture = cv2.VideoCapture(video_source)
-    assert capture.isOpened(), "Error reading video file"
-    return capture, video_source
+def write_video(capture, save_dir, exist_ok, fourcc, fps, width, height, resize):
+    if capture.isOpened():
+        file_path = increment_path(save_dir / "webcam.mp4", exist_ok=exist_ok)
+    video_writer = cv2.VideoWriter(
+        str(file_path),
+        fourcc,
+        fps,
+        (width, height),
+    )
+    if resize:
+        adjust_window_size(width, height)
+    return video_writer
 
 
 def adjust_window_size(width, height):
     aspect_ratio = width / height
     new_height = int(WINDOW_WIDTH / aspect_ratio)
     cv2.resizeWindow(WINDOW_NAME, WINDOW_WIDTH, new_height)
-
-
-def setup_output(capture, source, webcam, exist_ok, resize, save_img):
-    width, height, fps = int(capture.get(3)), int(capture.get(4)), int(capture.get(5))
-    if resize:
-        adjust_window_size(width, height)
-
-    if save_img:
-        save_dir = Path(OUTPUT_PATH)
-        save_dir.mkdir(parents=True, exist_ok=True)
-        fourcc = cv2.VideoWriter_fourcc(*"mp4v")
-        if not webcam:
-            file_path = increment_path(
-                save_dir / f"{Path(source).stem}.mp4", exist_ok=exist_ok
-            )
-        else:
-            file_path = increment_path(save_dir / "webcam.mp4", exist_ok=exist_ok)
-
-        return cv2.VideoWriter(
-            str(file_path),
-            fourcc,
-            fps,
-            (width, height),
-        )
-    return None
-
-
-def annotate_box(frame, annotator, box, names, cls, track_id, track_history, thickness):
-    annotator.box_label(box, str(names[cls]), color=colors(cls, True))
-    bbox_center = ((box[0] + box[2]) / 2, (box[1] + box[3]) / thickness)
-
-    track = track_history[track_id]
-    track.append((float(bbox_center[0]), float(bbox_center[1])))
-    if len(track) > 30:
-        track.pop(0)
-    points = np.hstack(track).reshape(-1, 1, 2).astype(np.int32)
-    cv2.polylines(frame, [points], False, colors(cls, True), thickness)
-    return bbox_center
-
-
-def annotate_frame(frame, results, track_history, thickness, counting_regions):
-    names = results[0].names
-    if results[0].boxes.id is not None:
-        boxes = results[0].boxes.xyxy
-        track_ids = results[0].boxes.id.tolist()
-        clss = results[0].boxes.cls.tolist()
-
-        annotator = Annotator(frame, line_width=2, example=str(names))
-
-        for box, track_id, cls in zip(boxes, track_ids, clss):
-            bbox_center = annotate_box(
-                frame,
-                annotator,
-                box,
-                names,
-                cls,
-                track_id,
-                track_history,
-                thickness,
-            )
-
-            for region in counting_regions:
-                if region["polygon"].contains(Point((bbox_center[0], bbox_center[1]))):
-                    region["counts"] += 1
-
-    for region in counting_regions:
-        region_label = str(region["counts"])
-        region_color = region["region_color"]
-        region_text_color = region["text_color"]
-
-        polygon_coords = np.array(region["polygon"].exterior.coords, np.int32)
-        centroid_x, centroid_y = (
-            int(region["polygon"].centroid.x),
-            int(region["polygon"].centroid.y),
-        )
-
-        text_size, _ = cv2.getTextSize(
-            region_label, cv2.FONT_HERSHEY_SIMPLEX, 1, thickness
-        )
-        text_x = centroid_x - text_size[0] // 2
-        text_y = centroid_y + text_size[1] // 2
-        cv2.rectangle(
-            frame,
-            (text_x - 5, text_y - text_size[1] - 5),
-            (text_x + text_size[0] + 5, text_y + 5),
-            region_color,
-            -1,
-        )
-        cv2.putText(
-            frame,
-            region_label,
-            (text_x, text_y),
-            cv2.FONT_HERSHEY_SIMPLEX,
-            1,
-            region_text_color,
-            thickness,
-        )
-        cv2.polylines(frame, [polygon_coords], True, region_color, thickness)
-
-
-def process_frame(frame, model, device, classes, track_history, thickness):
-    results = model.track(
-        frame, persist=True, show=False, device=device, classes=classes
-    )
-    annotate_frame(frame, results, track_history, thickness, counting_regions)
-
-
-def cleanup(save_img, video_writer, capture, count):
-    del count
-    if save_img:
-        video_writer.release()
-    capture.release()
-    cv2.destroyAllWindows()
 
 
 def main(
@@ -216,11 +104,40 @@ def main(
     thickness=2,
 ):
     count = 0
-    model, device = setup_model()
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model = YOLO(YOLO_MODEL)
+
+    names = model.model.names
+
     window_name = WINDOW_NAME
     cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
-    capture, source = setup_video_source(webcam)
-    video_writer = setup_output(capture, source, webcam, exist_ok, resize, save_img)
+
+    video_source = 0 if webcam else VIDEO_SOURCE
+    capture = cv2.VideoCapture(video_source)
+
+    assert capture.isOpened(), "Error reading video file"
+    width, height, fps = int(capture.get(3)), int(capture.get(4)), int(capture.get(5))
+
+    # Output setup
+    save_dir = Path("output")
+    save_dir.mkdir(parents=True, exist_ok=True)
+    fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+    if not webcam:
+        file_path = increment_path(
+            save_dir / f"{Path(video_source).stem}.mp4", exist_ok=exist_ok
+        )
+    else:
+        file_path = increment_path(save_dir / "webcam.mp4", exist_ok=exist_ok)
+    video_writer = cv2.VideoWriter(
+        str(file_path),
+        fourcc,
+        fps,
+        (width, height),
+    )
+    if resize:
+        aspect_ratio = width / height
+        new_height = int(WINDOW_WIDTH / aspect_ratio)
+        cv2.resizeWindow(window_name, WINDOW_WIDTH, new_height)
 
     while capture.isOpened():
         success, frame = capture.read()
@@ -233,7 +150,67 @@ def main(
             if count % speed != 0:
                 continue
 
-        process_frame(frame, model, device, classes, track_history, thickness)
+        results = model.track(
+            frame, persist=True, show=False, device=device, classes=classes
+        )
+
+        if results[0].boxes.id is not None:
+            boxes = results[0].boxes.xyxy
+            track_ids = results[0].boxes.id.tolist()
+            clss = results[0].boxes.cls.tolist()
+
+            annotator = Annotator(frame, line_width=2, example=str(names))
+
+            for box, track_id, cls in zip(boxes, track_ids, clss):
+                annotator.box_label(box, str(names[cls]), color=colors(cls, True))
+                bbox_center = ((box[0] + box[2]) / 2, (box[1] + box[3]) / thickness)
+
+                track = track_history[track_id]
+                track.append((float(bbox_center[0]), float(bbox_center[1])))
+                if len(track) > 30:
+                    track.pop(0)
+                points = np.hstack(track).reshape(-1, 1, 2).astype(np.int32)
+                cv2.polylines(frame, [points], False, colors(cls, True), thickness)
+
+                for region in counting_regions:
+                    if region["polygon"].contains(
+                        Point((bbox_center[0], bbox_center[1]))
+                    ):
+                        region["counts"] += 1
+
+        for region in counting_regions:
+            region_label = str(region["counts"])
+            region_color = region["region_color"]
+            region_text_color = region["text_color"]
+
+            polygon_coords = np.array(region["polygon"].exterior.coords, np.int32)
+            centroid_x, centroid_y = (
+                int(region["polygon"].centroid.x),
+                int(region["polygon"].centroid.y),
+            )
+
+            text_size, _ = cv2.getTextSize(
+                region_label, cv2.FONT_HERSHEY_SIMPLEX, 1, thickness
+            )
+            text_x = centroid_x - text_size[0] // 2
+            text_y = centroid_y + text_size[1] // 2
+            cv2.rectangle(
+                frame,
+                (text_x - 5, text_y - text_size[1] - 5),
+                (text_x + text_size[0] + 5, text_y + 5),
+                region_color,
+                -1,
+            )
+            cv2.putText(
+                frame,
+                region_label,
+                (text_x, text_y),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                1,
+                region_text_color,
+                thickness,
+            )
+            cv2.polylines(frame, [polygon_coords], True, region_color, thickness)
 
         if view_img:
             if count == 1:
@@ -249,7 +226,10 @@ def main(
         if cv2.waitKey(10) & 0xFF == ord("q"):
             break
 
-    cleanup(save_img, video_writer, capture, count)
+    del count
+    video_writer.release()
+    capture.release()
+    cv2.destroyAllWindows()
 
 
 def parse_options():
